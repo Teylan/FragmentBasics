c0ad3139041a1a482fbe93f4d13240cb744a2641
Brian Bansenauer <brianb@cascadia.edu>
2020-01-06 22:50:48 -0800

    setup TODOs and fragments

--- app/src/main/java/edu/cascadia/mobile/apps/fragmentbasics/FragmentB.java
+++ app/src/main/java/edu/cascadia/mobile/apps/fragmentbasics/FragmentB.java
@@ -13,13 +13,13 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentA extends Fragment {
+public class FragmentB extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false);
+        return inflater.inflate(R.layout.fragment_b, container, false);
     }
 
 }
--- app/src/main/java/edu/cascadia/mobile/apps/fragmentbasics/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobile/apps/fragmentbasics/MainActivity.java
@@ -1,28 +1,29 @@
 package edu.cascadia.mobile.apps.fragmentbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager fm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(savedInstanceState==null){
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.fragment_place,new FragmentA(),"fragA")
-                    .commit();
-        }
-
-        //TODO: Prepare an function for the button to load fragment B into its spot
-        // HINT: this is much the same as the transaction above except done on the button click
-
-
-        //TODO: Modify your onClick Listener for button above to replace fragmentA with fragmentB
-        // ALSO: put the current fragment onto the backstack
+        fm = getSupportFragmentManager();
+        fm.beginTransaction()
+                .add(R.id.fragment_place, new FragmentB(), "frag")
+                .commit();
     }
+    //TODO: Prepare an function for the button to load fragment B into the fragment place
+    // ALSO: put the current fragment onto the back stack and set a transition
+    public void onSwitchFrag(View view) {
+
+    }
+
+    //TODO: Override the onBackPressed() Activity method to make fragments respond to the "Back" button
+
 
 }
