bb82ce4299b51ed6c32540cfe4bf5fbcabd68cb0
Brian Bansenauer <brianb@cascadia.edu>
2020-01-06 17:46:23 -0800

    fragmentA

--- app/src/main/java/edu/cascadia/mobile/apps/fragmentbasics/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobile/apps/fragmentbasics/MainActivity.java
@@ -10,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState==null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_place,new FragmentA(),"fragA")
+                    .commit();
+        }
+
+        //TODO: Prepare an function for the button to load fragment B into its spot
+        // HINT: this is much the same as the transaction above except done on the button click
+
+
+        //TODO: Modify your onClick Listener for button above to replace fragmentA with fragmentB
+        // ALSO: put the current fragment onto the backstack
     }
+
 }
